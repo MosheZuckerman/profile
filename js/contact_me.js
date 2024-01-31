b923d6b15d327a61628b045b6384c6f38c51bf4c
Moshe Zuckerman <151771158+MosheZuckerman@users.noreply.github.com>
2024-01-31 16:03:49 +0200

    Add files via upload

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,80 +1,50 @@
 $(function() {
-    $("input,textarea").jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function($form, event, errors) {
-            // additional error messages or events
-        },
-        submitSuccess: function($form, event) {
-            event.preventDefault(); // prevent default submit behaviour
+    $('#contactForm').submit(function(e) {
+        e.preventDefault();
 
-            // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
+        var name = $('#name').val();
+        var email = $('#email').val();
+        var message = $('#message').val();
 
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
-
-            // Use Formspree AJAX library for handling form submissions
-            $.ajax({
-                url: "https://formspree.io/f/mayrwgvp",
-                method: "POST",
-                dataType: "json",
-                data: {
-                    name: name,
-                    email: email,
-                    message: message
-                },
-                success: function(response) {
-                    // Success message
-                    if (response.success) {
-                        $('#success').html("<div class='alert alert-success'>");
-                        $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                        $('#success > .alert-success')
-                            .append("<strong>Your message has been sent. </strong>");
-                        $('#success > .alert-success')
-                            .append('</div>');
-                    } else {
-                        // Fail message
-                        $('#success').html("<div class='alert alert-danger'>");
-                        $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                        $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", there was an issue. Please try again later!</strong>");
-                        $('#success > .alert-danger').append('</div>');
-                    }
-
-                    // Clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
+        $.ajax({
+            url: 'https://formspree.io/f/mayrwgvp',
+            method: 'POST',
+            dataType: 'json',
+            data: {
+                name: name,
+                email: email,
+                message: message
+            },
+            success: function(response) {
+                if (response.success) {
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+                } else {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", there was an issue. Please try again later!</strong>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + name + ", there was an issue. Please try again later!</strong>");
                     $('#success > .alert-danger').append('</div>');
+                }
 
-                    // Clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            });
-        },
-        filter: function() {
-            return $(this).is(":visible");
-        },
-    });
+                // Clear all fields
+                $('#contactForm').trigger('reset');
+            },
+            error: function() {
+                $('#success').html("<div class='alert alert-danger'>");
+                $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    .append("</button>");
+                $('#success > .alert-danger').append("<strong>Sorry " + name + ", there was an issue. Please try again later!</strong>");
+                $('#success > .alert-danger').append('</div>');
 
-    $("a[data-toggle=\"tab\"]").click(function(e) {
-        e.preventDefault();
-        $(this).tab("show");
-    });
-
-    /*When clicking on Full hide fail/success boxes */
-    $('#name').focus(function() {
-        $('#success').html('');
+                // Clear all fields
+                $('#contactForm').trigger('reset');
+            }
+        });
     });
 });
