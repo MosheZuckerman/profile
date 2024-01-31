2424d73c79624135aa8af213f82fcb3c630d5faa
Moshe Zuckerman <151771158+MosheZuckerman@users.noreply.github.com>
2024-01-31 14:59:46 +0200

    Add files via upload

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,11 +1,11 @@
-$(function() {
+$(function () {
     $("input,textarea").jqBootstrapValidation({
         preventSubmit: true,
-        submitError: function($form, event, errors) {
+        submitError: function ($form, event, errors) {
             // additional error messages or events
         },
-        submitSuccess: function($form, event) {
-            event.preventDefault(); // prevent default submit behaviour
+        submitSuccess: function ($form, event) {
+            event.preventDefault(); // prevent default submit behavior
 
             // get values from FORM
             var name = $("input#name").val();
@@ -28,7 +28,7 @@ $(function() {
                     message: message
                 },
                 cache: false,
-                success: function() {
+                success: function () {
                     // Success message
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
@@ -38,33 +38,33 @@ $(function() {
                     $('#success > .alert-success')
                         .append('</div>');
 
-                    //clear all fields
+                    // clear all fields
                     $('#contactForm').trigger("reset");
                 },
-                error: function() {
+                error: function () {
                     // Fail message
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that there was an issue. Please try again later!</strong>");
                     $('#success > .alert-danger').append('</div>');
-                    //clear all fields
+                    // clear all fields
                     $('#contactForm').trigger("reset");
                 },
             });
         },
-        filter: function() {
+        filter: function () {
             return $(this).is(":visible");
         },
     });
 
-    $("a[data-toggle=\"tab\"]").click(function(e) {
+    $("a[data-toggle=\"tab\"]").click(function (e) {
         e.preventDefault();
         $(this).tab("show");
     });
 
     /*When clicking on Full hide fail/success boxes */
-    $('#name').focus(function() {
+    $('#name').focus(function () {
         $('#success').html('');
     });
 });
